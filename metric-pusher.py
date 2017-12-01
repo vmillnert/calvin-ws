e3d556250e499196f4b8f76af092f871818b6dc5
Victor Millnert <victor@control.lth.se>
2017-12-01 12:26:05 +0100

    health triggered migration works

--- metric-pusher.py
+++ metric-pusher.py
@@ -12,7 +12,7 @@ class metricPusher:
         
         self.applications = []
         self.actors = []
-        
+        self.capabilities = []
 
     def get_node_id(self):
         resp = requests.get('http://' + self.curi + '/id')
@@ -21,8 +21,8 @@ class metricPusher:
 
         else:
             resp_json = resp.json()
-            self.id = resp_json['id']
-            print 'Node id: ' + self.id
+            self.node_id = resp_json['id']
+            print 'Node id: ' + self.node_id
 
 
     def get_application_ids(self):
@@ -44,9 +44,32 @@ class metricPusher:
         else:
             self.actors = resp.json()
             print "Actor ids: "
+            
             for actor in self.actors:
                 print actor
+ 
+    def get_capabilities(self):
+        resp = requests.get('http://' + self.curi + '/capabilities')
+        if resp.status_code != 200:
+            print('ERROR: ' + 'GET /capabilities {}'.format(resp.status_code))
 
+        else:
+            capabilities = resp.json()
+            print "capabilities: " + str(capabilities)
+
+            # print "Capabilities: "
+            # for capability in self.capabilities:
+            #     print capability
+               
+    def get_attributes(self):
+        resp = requests.get('http://' + self.curi + '/node/' + self.node_id )
+        if resp.status_code != 200:
+            print('ERROR: ' + 'GET /node/<node_id> {}'.format(resp.status_code))
+        else:
+            node_info = resp.json()
+            print "attributes: " + str(node_info['attributes'])
+
+                
     def set_health(self, value):
         
         data = {"value": value}
@@ -61,11 +84,68 @@ class metricPusher:
             print 'Updated health. ID: ' + resp.json()["id"]
 
 
+    def add_attribute(self):        
+        # data = {"index": ["user_extra", {"healthy": "yes"}]}
+        # data = {"node_name": {"name": "wasp"}}
+        data = {"health": "good"}
+        # data = [{"node_name": {}},
+        #         {"owner": {}},
+        #         {"address": {}},
+        #         {"user_extra": {"healthy": "yes"}}]
+        
+        resp = requests.post('http://' + self.curi + '/node/' + self.node_id + '/attributes/indexed_public',
+                             json=data)
+
+        if resp.status_code != 200:
+            print('ERROR: ' + 'POST /node/attributes/indexed_public {}'.format(resp.status_code))
+
+        else:
+            print 'Added attribute!'
+
+    def add_value(self):
+        
+        # data = {"index": ["user_extra", {"healthy": "yes"}]}
+        # data = {"node_name": {"name": "wasp"}}
+
+        data = {"value": "yes"}
+        
+        resp = requests.post('http://' + self.curi + '/index/healthy',
+                             json=data)
+
+        if resp.status_code != 200:
+            print('ERROR: ' + 'POST /index/healthy {}'.format(resp.status_code))
+
+        else:
+            print 'Added health-value!'
+
+    def get_value(self):
+        
+        # data = {"index": ["user_extra", {"healthy": "yes"}]}
+        # data = {"node_name": {"name": "wasp"}}
+
+        
+        resp = requests.get('http://' + self.curi + '/storage/health')
+
+        if resp.status_code != 200:
+            print('ERROR: ' + 'GET /storage/health {}'.format(resp.status_code))
+
+        else:
+            data = resp.json()
+            print 'Got health-value: '
+            print data
+
+
             
 if __name__ == "__main__":
 
     curi = sys.argv[1]
+    health = sys.argv[2]
     mp = metricPusher(curi)
     mp.get_application_ids()
     mp.get_actor_ids()
-    mp.set_health(0.5)
+    # mp.get_capabilities()
+    mp.set_health(health)
+    # mp.add_value()
+    # mp.add_attribute()
+    mp.get_attributes()
+    mp.get_value()
